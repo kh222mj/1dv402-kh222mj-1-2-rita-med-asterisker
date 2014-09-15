00a5ecc6fd85568442445f345f66753fc0d77284
kh222mj <kh222mj@lnu.se>
2014-09-15 22:37:55 +0200

    klar med uppgift 1-2

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int row = 0; row < 25; row++)
+            {
+                
+                switch(row % 3)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                }
+                
+                
+                if (row % 2 == 1)
+                {
+                    Console.Write(" ");
+
+
+                }
+                for (int col = 0; col < 39; col++)
+                {
+
+                    Console.Write("* ");
+
+
+                }
+
+                Console.WriteLine();
+            }  
+
+        }
+    }
+}
